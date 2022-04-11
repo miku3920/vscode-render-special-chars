27ffe92d80963087be89af565bcfc59fc929b389
miku3920 <papa964204@gamil.com>
2022-04-12 01:19:18 +0800

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# vscode-render-special-chars
-Displays any characters with UTFx
+Render Special Characters
+=========================
+
+This extension shows any characters with UTFx. 
+
+## Features
+
+![Screenshot](https://raw.githubusercontent.com/miku3920/vscode-render-special-chars/main/images/example.png)
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `render-special-chars.include`: Array of UTFx or character. This characters will be shown.
+
+* `render-special-chars.except`: Array of UTFx or character. Characters will be shown with "include" except "except".
+
+* `render-special-chars.border`: CSS string.
+
+* `render-special-chars.borderColor`: HTML color code.
+
+* `render-special-chars.color`: HTML color code.
+
+* `render-special-chars.backgroundColor`: HTML color code.
+
+Color is taken from `editorUnicodeHighlight.border` theme color.
+BackgroundColor is taken from `editorUnicodeHighlight.background` theme color.
+
+### Default Configuration
+
+```json
+"render-special-chars.include": [
+   "0000-0009",
+   "000B",
+   "000C",
+   "000E-001F",
+   "007F-009F",
+   "00A0-4DFF",
+   "A000-10FFFF"
+],
+"render-special-chars.except": [
+   "000A",
+   "000D",
+   "0020-007E",
+   "4E00-9FFF"
+],
+"render-special-chars.border": "1px solid; border-radius: 2px;",
+"render-special-chars.borderColor": null,
+"render-special-chars.color": null,
+"render-special-chars.backgroundColor": null,
+```
---
+++ out/extension.js
@@ -0,0 +1,116 @@
+'use strict'
+
+const vscode = require('vscode')
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	let timeout = null
+
+	/**
+	 * @param {Array} array
+	 */
+	function createRegEx(array) {
+		const numbers = new Array(10).fill(true)
+		const characters = array.map(item => {
+			return item.trim().split('-').map((/** @type {string} */ value) => {
+				value = value.trim()
+				const code = parseInt(value, 16)
+
+				return (Number.isInteger(code) && !numbers[value]) ? String.fromCodePoint(code) : value
+			}).join('-')
+		}).join('')
+		return new RegExp('[' + characters + ']', 'ug')
+	}
+
+	const defaultColor = new vscode.ThemeColor('editorUnicodeHighlight.border')
+	const defaultBackgroundColor = new vscode.ThemeColor('editorUnicodeHighlight.background')
+	const customConfiguration = vscode.workspace.getConfiguration('render-special-chars')
+	const borderConfig = customConfiguration.get('border')
+	const borderColor = customConfiguration.get('borderColor') || defaultColor
+	const textColor = customConfiguration.get('color') || defaultColor
+	const backgroundColor = customConfiguration.get('backgroundColor') || defaultBackgroundColor
+	const include = createRegEx(customConfiguration.get('include'))
+	const except = createRegEx(customConfiguration.get('except'))
+
+	const decorationTypes = {}
+
+	/**
+	 * @param {String} char
+	 */
+	function getDecorationType(char) {
+		let contentText = char.codePointAt(0).toString(16).toUpperCase()
+		decorationTypes[contentText] = decorationTypes[contentText] || vscode.window.createTextEditorDecorationType({
+			after: {
+				contentText: contentText,
+				color: textColor,
+				border: borderConfig,
+				borderColor: borderColor,
+				backgroundColor: backgroundColor
+			},
+			rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed
+		})
+		return decorationTypes[contentText]
+	}
+
+	/** 
+	 * @param {vscode.TextEditor} editor 
+	 */
+	function updateDecorations(editor) {
+		if (!editor) { return }
+
+		const text = editor.document.getText()
+		const decorations = {}
+
+		let match
+		while ((match = include.exec(text))) {
+			if (match[0].match(except)) { continue }
+
+			const pos = editor.document.positionAt(match.index)
+
+			decorations[match[0]] = decorations[match[0]] || []
+			decorations[match[0]].push({ range: new vscode.Range(pos, pos) })
+		}
+		for (const [key, value] of Object.entries(decorations)) {
+			editor.setDecorations(getDecorationType(key), value)
+		}
+
+		clearTimeout(timeout)
+		timeout = null
+	}
+
+	/** 
+	 * @param {vscode.TextEditor} editor
+	 */
+	function triggerUpdateDecorations(editor, throttle = false) {
+		if (!throttle) {
+			updateDecorations(editor)
+			return
+		}
+		if (!timeout) {
+			timeout = setTimeout(updateDecorations, 500, editor)
+		}
+		return
+	}
+
+	triggerUpdateDecorations(vscode.window.activeTextEditor)
+
+	vscode.window.onDidChangeActiveTextEditor((/** @type {vscode.TextEditor} */ editor) => {
+		triggerUpdateDecorations(editor)
+	}, null, context.subscriptions)
+
+	vscode.workspace.onDidChangeTextDocument((/** @type {vscode.TextDocumentChangeEvent} */ event) => {
+		if (event.contentChanges.length > 0) {
+			triggerUpdateDecorations(vscode.window.activeTextEditor, true)
+		}
+	}, null, context.subscriptions)
+}
+
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
